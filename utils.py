0ddf4d3987361e47339e72564aa60929bf67721a
sebpost2 <81270882+sebpost2@users.noreply.github.com>
2024-10-06 20:48:16 -0500

    Refactoring
    Se eliminaron archivos innecesarios:
    main.py
    Instructions.py
    gui.py
    yolov5spt
    Backups

--- utils.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import messagebox
-
-def show_credits():
-    # Código para mostrar créditos
-    pass
-
-def show_info():
-    messagebox.showinfo("Project Information", "This project uses OpenCV to capture video and estimate body poses.")
